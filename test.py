ce1715337f1305a41c1e852424b8d6b235549f09
Peter Chow <peterchowacademy@gmail.com>
2021-11-05 00:30:16 +0800

    new

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import string
-alphabet = set(string.ascii_uppercase)
-print(alphabet)
+# import string
+# alphabet = set(string.ascii_uppercase)
+# print(alphabet)
+
+###how to display the content from dictionary/json
+dict = {
+    "title" : "this is a title",
+    "content" : "this is a content"
+}
+print(dict["content"])
+print(dict['title'])
+print(f"title is {dict['title']} and content is {dict['content']}")
