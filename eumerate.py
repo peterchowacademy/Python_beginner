4e0c8c98ea8ab048e49418eb9b83f0cc1c312dc4
Peter Chow <peterchowacademy@gmail.com>
2021-11-12 00:26:01 +0800

    new

--- eumerate.py
+++ eumerate.py
@@ -7,3 +7,12 @@ print("end")
 
 for count, value in enumerate(values):
      print(count, value)
+
+print(" ")
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favouriate food", "content": "I like pizza", "id": 2}]
+
+for p in my_posts:
+    if p["id"] == id:
+        print(p)
